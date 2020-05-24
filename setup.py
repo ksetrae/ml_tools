ae986af6be37235ae42fdbeb2d2cb732c27c16dd
ksetrae <ahisaw@gmail.com>
2020-05-24 21:11:04 +0500

    Update setup and version

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 
-# with open('README.rst') as f:
-#     readme = f.read()
+with open('README.md') as f:
+    readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    license_ = f.read()
 
 setup(
-    name='ml_tools',
-    version='0.0.2',
-    description='',
-    long_description='',
+    name='sklearn_search_tools',
+    version='0.0.3',
+    description='Tools for tuning the hyper-parameters that are based on scikit-learn modules',
+    long_description=readme,
     author='ksetrae',
-    author_email='ksetrae@gmail.com',
-    license=license,
-    url='https://github.com/ksetrae/ml_tools.git',
+    author_email='ahisaw@gmail.com',
+    license=license_,
+    url='https://github.com/ksetrae/sklearn_search_tools.git',
     packages=find_packages()
 )
